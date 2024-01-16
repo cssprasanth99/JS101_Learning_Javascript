eba8d3930bb36fa55098f08ba8db3e915e489d10
cssprasanth99 <cssprasanth99@gmail.com>
2024-01-16 22:48:15 +0530

    folder rename

---
+++ Introduction_to_Javascript/Problem_2/index.js
@@ -0,0 +1,6 @@
+var name = "Prasanth";
+console.log("Hello " + name);
+name = "Prakasa rao";
+console.log("Hello " + name);
+name = "Vani";
+console.log("Hello " + name);
---
+++ Introduction_to_Javascript/Problem_5/index.js
@@ -0,0 +1,3 @@
+var num1 = 3, num2 = 2;
+var compare = (num1 == num2);
+console.log(compare);
